2f5c17136802a6918cae5b5d35709136b3124228
blackeblack312 <64986184+blackeblack312@users.noreply.github.com>
2020-12-06 20:05:27 +0500

    hi bay

--- python.py
+++
@@ -1,9 +0,0 @@
-magicians = ['alice', 'david', 'carolina']  
-magicans_2= ['sami' , 'rohan' ] 
-for magician in magicians:  
- for magician_2 in magicians:    
-     print(magician.title() + ", that was a great trick!")  
-     print("I can't wait to see your next trick, " + magician.title() + ".\n")
-print("I am boi")
- 
-
